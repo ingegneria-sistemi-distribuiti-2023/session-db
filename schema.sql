e6f169bc76e465d9cf2ee393279dc143e95db4e9
m.bongiovanni <m.bongiovanni@mongibello.tech>
2023-01-12 22:35:00 +0100

    fix wrong primary key in sessondb

--- schema.sql
+++ schema.sql
@@ -27,12 +27,12 @@ USE `sessiondb` ;
 -- Table `sessiondb`.`session`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sessiondb`.`session` (
-    `id` VARCHAR(255) NOT NULL,
+    `id` INTEGER auto_increment NOT NULL,
     `user_id` INT NOT NULL,
     `session_id` VARCHAR(255) NULL,
     `start_time` TIMESTAMP NOT NULL,
     `end_time` TIMESTAMP NULL DEFAULT NULL,
-    PRIMARY KEY (`id`))
+    CONSTRAINT `team_PK` PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
